ea1f1439d6558d4d87db821d92192a02fd0e3fab
mahmoud mokhtar <mahmoud01234elbalhi@gmail.com>
2024-11-02 00:22:27 +0200

    done

--- online_store_using_hooks_/src/data/branches.js
+++ online_store_using_hooks_/src/data/branches.js
@@ -3,54 +3,54 @@ export const branches = [
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhzeNQFPnktfDSIQfjcH32fx6tUaD4dPDV_Q&usqp=CAU",
     branch_name: "قسم الإلكترونيتات",
     txt: "أظهر منتجات هذا القسم",
-    link:'./pages/Electronics'
+    link:'/pages/Electronics'
   },
   {id:2,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXM9F_zbQAyu4MOLND2Io_SZP36zIo60Br3w&usqp=CAU",
     branch_name: "قسم الملابس",
     txt: "أظهر منتجات هذا القسم",
-    link:'./pages/Clothes'
+    link:'/pages/Clothes'
   },
   {id:3,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdt3yAy1X736M7mBlu86yRa_AxPEPSRxFDxQ&usqp=CAU",
     branch_name: "قسم الأجهزة الكهربائية",
     txt: "أظهر منتجات هذا القسم",
-    link:'./pages/Devices'
+    link:'/pages/Devices'
   },
   {id:4,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvwz74_N2QmzuCiFIQdiy79NEhFw2NhajbSg&usqp=CAU",
     branch_name: "قسم العدد",
     txt: "أظهر منتجات هذا القسم",
-    link:'./pages/Tools'
+    link:'/pages/Tools'
   },
   {id:5,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToJ8TPqPMDvQV7xPVONyyKsx49xRaAJluWQQ&usqp=CAU",
     branch_name: "منتجات السوبر ماركت",
     txt: "أظهر منتجات هذا القسم",
-    link:'./pages/Market'
+    link:'/pages/Market'
   },
   {id:6,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBWociss1PRJdHAd3ypB7VHirfVR9-8QZzkA&usqp=CAU",
     branch_name: "قسم الساعات",
     txt: "أظهر منتجات هذا القسم",
-    link:'./pages/Watches'
+    link:'/pages/Watches'
   },
   {id:7,
     img: "https://images-eu.ssl-images-amazon.com/images/G/42/Egypt-hq/2022/img/OHL/Gateway/XCM_Manual_1466755_4982783_758x608_2X._SY608_CB609289554_.jpg",
     branch_name: "قسم لعب الاطفال",
     txt: "أظهر منتجات هذا القسم",
-    link:'./pages/toyes'
+    link:'/pages/toyes'
   },
   {id:8,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvtLj8URKQ3Px93iPa5YIvNKp9rvwPehSgOw&usqp=CAU",
     branch_name: "قسم الأثاث",
     txt: "أظهر منتجات هذا القسم",
-    link:'./pages/Furniture'
+    link:'/pages/Furniture'
   },
   {id:9,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsnXSuyCmVh369yDeKd1z1ciu5viz2Wph2-A&usqp=CAU",
     branch_name: "قسم العناية بالشعر",
     txt: "أظهر منتجات هذا القسم",
-    link:'./pages/HairsTools'
+    link:'/pages/HairsTools'
   },
 ];
